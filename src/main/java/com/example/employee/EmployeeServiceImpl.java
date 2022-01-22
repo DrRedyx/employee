615da6b0ae98d39bb0523d01d7396152e908d25b
Redyx <dr.redyx@gmail.com>
2022-01-23 01:40:35 +0300

    StringUtils

--- src/main/java/com/example/employee/EmployeeServiceImpl.java
+++ src/main/java/com/example/employee/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.employee;
 
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 
@@ -21,6 +22,9 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public Employee addEmployee(String firstname, String lastname, int salary, int department) {
         Employee employee = new Employee(firstname, lastname, salary, department);
+        if (StringUtils.isAlpha(firstname) && StringUtils.isAlpha(lastname)) {
+            StringUtils.capitalize(firstname);
+            StringUtils.capitalize(lastname);
             if (!employees.contains(employee)) {
                 employees.add(employee);
                 return employee;
@@ -28,23 +32,41 @@ public class EmployeeServiceImpl implements EmployeeService {
                 throw new EmployeeIsAlreadyWorkException();
             }
         }
+        else {
+            throw new StringIsNotAlphabeticException();
+        }
+    }
     @Override
     public Employee deleteEmployee(String firstname, String lastname, int salary, int department) {
         Employee employee = new Employee(firstname, lastname, salary, department);
-        if (!employees.contains(employee)) {
-            employees.remove(employee);
-            return employee;
-        } else {
-            throw new EmployeeNotFindException();
+        if (StringUtils.isAlpha(firstname) && StringUtils.isAlpha(lastname)) {
+            StringUtils.capitalize(firstname);
+            StringUtils.capitalize(lastname);
+            if (!employees.contains(employee)) {
+                employees.remove(employee);
+                return employee;
+            } else {
+                throw new EmployeeNotFindException();
+            }
+        }
+        else {
+            throw new StringIsNotAlphabeticException();
         }
     }
     @Override
     public Employee findEmployee (String firstname, String lastname, int salary, int department){
         Employee employee = new Employee(firstname, lastname, salary, department);
-        if (!employees.contains(employee)) {
-            return employee;
-        } else {
-            throw new EmployeeNotFindException();
+        if (StringUtils.isAlpha(firstname) && StringUtils.isAlpha(lastname)) {
+            StringUtils.capitalize(firstname);
+            StringUtils.capitalize(lastname);
+            if (!employees.contains(employee)) {
+                return employee;
+            } else {
+                throw new EmployeeNotFindException();
+            }
+        }
+        else {
+            throw new StringIsNotAlphabeticException();
         }
     }
 
